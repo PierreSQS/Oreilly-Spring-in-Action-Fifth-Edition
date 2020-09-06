14702c34aeb1a40742b29dc0125783f13e3d70cb
PierreSQS <nomo_messina@web.de>
2020-09-06 14:21:46 +0200

    Commit ch02-04 OK: Extended Test new OrderController and assertions
    templates for the future.

--- ch02/taco-cloud/src/test/java/com/pierrot/demo/tacos/TacoCloudApplicationTests.java
+++ ch02/taco-cloud/src/test/java/com/pierrot/demo/tacos/TacoCloudApplicationTests.java
@@ -2,7 +2,10 @@ package com.pierrot.demo.tacos;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
@@ -12,8 +15,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.pierrot.demo.tacos.web.DesignTacoController;
+import com.pierrot.demo.tacos.web.OrderController;
 
-@WebMvcTest(controllers = {HomeController.class, DesignTacoController.class})
+@WebMvcTest(controllers = {HomeController.class, DesignTacoController.class, OrderController.class})
 class TacoCloudApplicationTests {
 	
 	@Autowired
@@ -35,4 +39,23 @@ class TacoCloudApplicationTests {
 		.andExpect(content().string(containsString("Design your taco")));
 	}
 
+	@Test
+	void testShowDesignForm_DesignTacoController() throws Exception {
+		mockMvc.perform(post("/design"))
+//				.param("name", "Top Gun")) // not for this form
+		.andExpect(status().is3xxRedirection())
+//		.andReturn() // to check what assertion does
+		.andExpect(view().name("redirect:/orders/current"))
+		.andExpect(redirectedUrl("/orders/current"));
+//		.andDo(print()); // to print the Results by all means
+	}
+
+	@Test
+	void testOrderForm_OrderController() throws Exception {
+		mockMvc.perform(get("/orders/current"))
+		.andExpect(model().attributeExists("order"))
+		.andExpect(status().isOk())
+		.andExpect(view().name("orderForm"))
+		.andExpect(content().string(containsString("Design another taco")));
+	}
 }
