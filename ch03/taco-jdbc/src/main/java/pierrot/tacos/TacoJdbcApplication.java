d5850e01e5b18356d7fd7332fb42160b035e7745
PierreSQS <nomo_messina@web.de>
2021-01-04 15:14:02 +0100

    Commit ch03-07 OK: Deleted HomeController and Moved Business-Logik to
    TacoJdbcApplication. Also miscellaneous formatting.

--- ch03/taco-jdbc/src/main/java/pierrot/tacos/TacoJdbcApplication.java
+++ ch03/taco-jdbc/src/main/java/pierrot/tacos/TacoJdbcApplication.java
@@ -2,12 +2,20 @@ package pierrot.tacos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class TacoJdbcApplication {
+public class TacoJdbcApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TacoJdbcApplication.class, args);
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		WebMvcConfigurer.super.addViewControllers(registry);
+		registry.addViewController("/").setViewName("home");
 	}
 
 }
