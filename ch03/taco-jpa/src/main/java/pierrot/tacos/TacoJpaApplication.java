97ac50ca95d6bfad41c545e8ef24b4ac63d40674
PierreSQS <nomo_messina@web.de>
2021-01-17 17:43:00 +0100

    Commit ch03-36 OK: JPA without data.sql. This is the final Version of
    the chapter

--- ch03/taco-jpa/src/main/java/pierrot/tacos/TacoJpaApplication.java
+++ ch03/taco-jpa/src/main/java/pierrot/tacos/TacoJpaApplication.java
@@ -1,10 +1,18 @@
 package pierrot.tacos;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import lombok.extern.slf4j.Slf4j;
+import pierrot.tacos.domain.Ingredient;
+import pierrot.tacos.domain.Ingredient.Type;
+import pierrot.tacos.repositories.IngredientRepository;
+
+@Slf4j
 @SpringBootApplication
 public class TacoJpaApplication implements WebMvcConfigurer {
 
@@ -15,6 +23,23 @@ public class TacoJpaApplication implements WebMvcConfigurer {
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("home");
+	}
+	
+	@Bean
+	public CommandLineRunner ingredientsLoaderToDB(IngredientRepository ingredientRepo) {
+		return args -> {
+			log.info("Loading Ingredients to the DB...");
+			ingredientRepo.save(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
+			ingredientRepo.save(new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
+			ingredientRepo.save(new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
+			ingredientRepo.save(new Ingredient("CARN", "Carnitas", Type.PROTEIN));
+			ingredientRepo.save(new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
+			ingredientRepo.save(new Ingredient("LETC", "Lettuce", Type.VEGGIES));
+			ingredientRepo.save(new Ingredient("CHED", "Cheddar", Type.CHEESE));
+			ingredientRepo.save(new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
+			ingredientRepo.save(new Ingredient("SLSA", "Salsa", Type.SAUCE));
+			ingredientRepo.save(new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+		};
 	}
 
 }
