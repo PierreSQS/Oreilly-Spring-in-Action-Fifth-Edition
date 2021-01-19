bf436d1d16e833cc0d8e3db115ebff202e6ff2ec
PierreSQS <nomo_messina@web.de>
2021-01-19 16:56:49 +0100

    Commit ch04-11 OK: Code with commented out Debug-Skeleton

--- ch04/taco-security/src/main/java/pierrot/tacos/TacoSecurityApplication.java
+++ ch04/taco-security/src/main/java/pierrot/tacos/TacoSecurityApplication.java
@@ -1,9 +1,11 @@
 package pierrot.tacos;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class TacoSecurityApplication  implements WebMvcConfigurer {
@@ -16,5 +18,15 @@ public class TacoSecurityApplication  implements WebMvcConfigurer {
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("home");
 	}
+	
+//	@Bean
+//	public CommandLineRunner showBeans(ApplicationContext appCtx) {
+//		return (args) -> {
+//			log.info("following the Beans provided by Spring...");
+//			String[] beanNames = appCtx.getBeanDefinitionNames();
+//			Arrays.asList(beanNames).forEach(bn ->  log.info("{}",bn));
+//			log.info("finally the total of Beans: {}", appCtx.getBeanDefinitionCount());
+//		};
+//	}
 
 }
