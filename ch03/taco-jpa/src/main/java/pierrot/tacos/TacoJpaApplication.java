e3c016587e606321faee7e617efc04cd03f23438
PierreSQS <nomo_messina@web.de>
2021-01-23 17:17:32 +0100

    Commit ch02-02-jpa-trials OK: Now the Ingredients on the DesignForm are
    displayed!!

--- ch03/taco-jpa/src/main/java/pierrot/tacos/TacoJpaApplication.java
+++ ch03/taco-jpa/src/main/java/pierrot/tacos/TacoJpaApplication.java
@@ -25,21 +25,21 @@ public class TacoJpaApplication implements WebMvcConfigurer {
 		registry.addViewController("/").setViewName("home");
 	}
 	
-	@Bean
-	public CommandLineRunner ingredientsLoaderToDB(IngredientRepository ingredientRepo) {
-		return args -> {
-			log.info("Loading Ingredients to the DB...");
-			ingredientRepo.save(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
-			ingredientRepo.save(new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
-			ingredientRepo.save(new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
-			ingredientRepo.save(new Ingredient("CARN", "Carnitas", Type.PROTEIN));
-			ingredientRepo.save(new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
-			ingredientRepo.save(new Ingredient("LETC", "Lettuce", Type.VEGGIES));
-			ingredientRepo.save(new Ingredient("CHED", "Cheddar", Type.CHEESE));
-			ingredientRepo.save(new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
-			ingredientRepo.save(new Ingredient("SLSA", "Salsa", Type.SAUCE));
-			ingredientRepo.save(new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
-		};
-	}
+//	@Bean
+//	public CommandLineRunner ingredientsLoaderToDB(IngredientRepository ingredientRepo) {
+//		return args -> {
+//			log.info("Loading Ingredients to the DB...");
+//			ingredientRepo.save(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
+//			ingredientRepo.save(new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
+//			ingredientRepo.save(new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
+//			ingredientRepo.save(new Ingredient("CARN", "Carnitas", Type.PROTEIN));
+//			ingredientRepo.save(new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
+//			ingredientRepo.save(new Ingredient("LETC", "Lettuce", Type.VEGGIES));
+//			ingredientRepo.save(new Ingredient("CHED", "Cheddar", Type.CHEESE));
+//			ingredientRepo.save(new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
+//			ingredientRepo.save(new Ingredient("SLSA", "Salsa", Type.SAUCE));
+//			ingredientRepo.save(new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+//		};
+//	}
 
 }
